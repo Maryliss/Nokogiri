055979ec335307fd9a36014b081c0762370c4b2d
Maxime Smolis <mpsmolis@gmail.com>
2019-01-17 20:23:23 +0100

    fichier suppr

--- lib/trader_obscur.rb
+++ lib/trader_obscur.rb
@@ -6,55 +6,49 @@ page = Nokogiri::HTML(open('https://coinmarketcap.com/all/views/all/'))
 #all_symbol_names = page.xpath('//*[@class="text-left col-symbol"]')
 #all_prices = page.xpath('//*[@class="price"]')
 
-all_symbol_names = page.xpath('//*/td[3]')
-all_prices = page.xpath('//*/td[5]/a')
+def trading (page)
+
+    # récuperation des symbol des cours des cryptomonnaies
+    all_symbol_names = page.xpath('//*/td[3]')
+
+    # récuperation des cours des cryptomonnaies
+    all_prices = page.xpath('//*/td[5]/a')
 
 
+    # création d'un array avec les symboles
+    array_symbol_names = []
+    all_symbol_names.each do |name|
+        array_symbol_names << name.text
+    end
 
-array_symbol_names = []
-all_symbol_names.each do |name|
-    array_symbol_names << name.text
+    # création d'un array avec les cours
+    array_prices = []
+    all_prices.each do |price|
+        array_prices << price.text
+    end
+
+    # création d'un hash avec en clé les symboles et en valeurs les cours
+    currencies = {} 
+    array_symbol_names.zip(array_prices) {|a,b| currencies[a] = b }
+
+    # creation d'un array global qui contiendra les hashs pour chaque crypto
+    array_of_hashes = []
+
+    # suppression du premier caractère = $
+    currencies.each {|k, v| v[0] = ""}
+
+    # conversion en float (décimal)
+    currencies.transform_values!(&:to_f)
+
+    # creation des hashs individuel et on les stock dans l'array global (array_of_hashes)
+    currencies.each do |key, value|
+        hash = Hash.new
+        hash[key] = value
+        array_of_hashes << hash
+    end
+
+    print array_of_hashes
+
 end
 
-
-array_prices = []
-all_prices.each do |price|
-    array_prices << price.text
-end
-
-
- currencies = {} 
- array_symbol_names.zip(array_prices) {|a,b| currencies[a] = b }
-
- array_of_hashes = []
-
-
-currencies.each {|k, v| v[0] = ""}
-currencies.transform_values!(&:to_f)
-
-
-currencies.each do |key, value|
-    hash = Hash.new
-    hash[key] = value
-    array_of_hashes << hash
-end
-
-print array_of_hashes
-
-
-
-
-
-
-
-
-
-
-
-
-# final_table = []
-# array_of_arrays.each do |r| 
-#     final_table << {array_symbol_names[0] => array_prices[0]}
-# end
-
-# print final_table
+trading (page)
---
+++ spec/trader_obscur_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/trader_obscur'
+
+describe "The trading method" do
+    it "should get the symbol and price of each cryptocurrencies, return an array with nested hashes for each cryptocurrencies and not to be nil" do
+        expect(trading).not_to be_nil
+    end
+  end
