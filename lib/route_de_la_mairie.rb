13fa1d7ec16ca034bb4eee7c900ebdd70b2f9f82
Maxime Smolis <mpsmolis@gmail.com>
2019-01-17 19:56:28 +0100

    exo

--- lib/route_de_la_mairie.rb
+++ lib/route_de_la_mairie.rb
@@ -2,36 +2,78 @@ require 'rubygems'
 require 'nokogiri'   
 require 'open-uri'
 
-page_ville = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
+# page liste des villes 
+page_villes = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
 
-page = Nokogiri::HTML(open('https://www.annuaire-des-mairies.com/95/avernes.html'))
 
-e_mail = page.xpath('//*/tr[4]/td[2]')
+# page de la ville d'averne
+townhall_url = Nokogiri::HTML(open('https://www.annuaire-des-mairies.com/95/avernes.html'))
 
-# print e_mail.text
+# choper l'email d'une ville
+def get_townhall_email(townhall_url)
+    
+    a = townhall_url.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+    
+    puts a.text
+end
 
-ville = page_ville.xpath('//p/a')
 
-ville.each do |x|
-    puts x.text
+# choper les URLS de chaque ville 
+def get_townhall_urls (page_villes)
+    lien = page_villes.xpath('//*/p/a/@href')
+end
+
+
+# creation array
+array_url = []
+# stock les url dans tableau 
+get_townhall_urls(page_villes).each do |lien|
+    array_url << lien.text
 end
 
 
 
-# ville.each do |x|
-#     puts x.text
-# end
+# suppr le premier caractere de chaque element du array (.)
+array_url.each {|v| v[0] = ""}
 
-# def get_townhall_email (townhall_url)
+# assemblage de l'URL de base avec element du array_villes.
+
+array_emails = []
+array_url.each do |x|
+    url = Nokogiri::HTML(open("https://www.annuaire-des-mairies.com" + x))
+    email = url.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+    array_emails << email.text
+end
 
 
 
+#Affiche les noms de villes
+def get_city_names (page_villes)
+    lien = page_villes.xpath('//*/td/p/a')
+end
 
 
-# end 
+get_city_names (page_villes)
+
+#création d'un tableau avec les noms de villes
+array_city_names = []
+    get_city_names(page_villes).each do |name|
+        array_city_names << name.text
+    end
+    # puts array_city_names
 
 
+#création d'un hash avec les noms de villes et les mails
+hash_annuaire = {}
+array_city_names.zip(array_emails) {|a,b| hash_annuaire[a] = b }
 
-                        
 
-                        
+array_final = []
+
+hash_annuaire.each do |key, value|
+    array_annuaire = Hash.new
+    array_annuaire[key] = value
+    array_final << array_annuaire
+end
+
+puts array_final
